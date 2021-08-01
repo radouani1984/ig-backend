08395a248972a77d2cf97405978da8195e17256b
Famille Adouani <familleadouani2019@gmail.com>
2021-08-01 22:31:25 +0200

    initial strapi backend project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed136c838854b118290dbb24b092dfa6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ce7879b7-d8f1-4a90-9527-f52b2cdb859b'
+};
